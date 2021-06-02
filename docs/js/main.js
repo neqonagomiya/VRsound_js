6b681f309b8ca4855f58fc6cdabf58e5e671ed48
neqonagomiya <neqofan@gmail.com>
2021-06-02 15:22:55 +0900

    fix main.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,22 +1,22 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 let oscillator;
-// isPlaying is True when sound play
+// 再生中でtrue
 let isPlaying = false;
 
 document.querySelector("#play").addEventListener("click", () => {
-	// If it is playing, prevent it from being played twice.
-	if (isPlaying) return;
-	// setting oscillator.
-	oscillator = ctx.createOscillator();
-	oscillator.type = "sine";
-	oscillator.frequency.setValueAtTime(440,ctx.currentTime);
-	oscillator.connect(ctx.destination);
-	oscillator.start();
-	isPlaying = true;
+  // 再生中なら二重に再生されないようにする
+  if(isPlaying) return;
+  oscillator = ctx.createOscillator();
+  oscillator.type = "sine"; // sine, square, sawtooth, triangleがある
+  oscillator.frequency.setValueAtTime(440, ctx.currentTime); // 440HzはA4(4番目のラ)
+  oscillator.connect(ctx.destination);
+  oscillator.start();
+  isPlaying = true
 });
 
+// oscillatorを破棄し再生を停止する
 document.querySelector("#stop").addEventListener("click", () => {
-	oscillator?.stop();
-	isPlaying = false;
+  oscillator?.stop();
+  isPlaying = false;
 });
